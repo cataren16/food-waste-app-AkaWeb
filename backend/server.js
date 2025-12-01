a3fe1f04754a6e9ca6278ec825592972c13327d4
Rasmerita Andra <andrarsmr@gmail.com>
2025-12-01 20:19:38 +0200

    implementat backend produse(controller, rute, server)

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const db = require('./models'); 
 const friendRoutes = require('./routes/friendRoutes');
 const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/api/friends',friendRoutes)
+app.use('/api/friends',friendRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Serverul food-waste-app-akaweb ruleaza cu succes!!!');
