726ee77cc13b54c01f03a7f4e02795429d0eab59
Ancarosoiu <anca.rosoiu04@gmail.com>
2026-01-13 18:09:32 +0200

    incercare deploy 1

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 
